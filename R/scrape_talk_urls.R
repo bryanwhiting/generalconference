ce229b87be9c6ba9ac2a695af03edeffbb764a84
Bryan Whiting <bryan.g.whiting@gmail.com>
2021-08-21 16:38:25 -0700

    Updated scrape_talk_urls

--- R/scrape_talk_urls.R
+++ R/scrape_talk_urls.R
@@ -8,16 +8,16 @@ file.path(url_root, 1971, "04", "?lang=eng")
      
 # output: dataframe with session url and nested df of all urls for that session
 url = file.path(url_root, 1971, "04", "?lang=eng")
+# url <- 'https://www.churchofjesuschrist.org/study/general-conference/1971/04/out-of-the-darkness'
 html_document <- rvest::read_html(url)
 a <- html_document %>% html_element(".doc-map") 
+a %>%
+  html_elements('.title') %>%
+  html_text
+
 a %>%
   html_nodes('a') %>%
   html_attr('href')
 
 
-
-# Todo: fix the import and add unit tests
-url <- 'https://www.churchofjesuschrist.org/study/general-conference/1971/04/out-of-the-darkness'
-scrape_talk(url) %>%
-  tidyr::unnest(paragraphs)
   
